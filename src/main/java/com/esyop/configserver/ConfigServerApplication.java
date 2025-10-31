9798c384dc6cafbdd53f2661feaf67b43f645ebf
Rafael Alvarez <rafaelrio4@gmail.com>
2025-10-30 22:10:57 -0300

    commit

--- src/main/java/com/esyop/configserver/ConfigServerApplication.java
+++ src/main/java/com/esyop/configserver/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.esyop.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,7 +9,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
+
     public static void main(String[] args) {
+
         SpringApplication.run(ConfigServerApplication.class, args);
+        logger.info("***********App iniciada*****");
     }
 }
